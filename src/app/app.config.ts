4a35c1e4a919436d4b43b8e10df962fd584a0997
Steven Muñoz <darkmixt@gmail.com>
2024-12-23 12:42:23 -0600

    feat: redux devtools added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './store/marcador/marcador-reducer/marcador-reducer.component';
 import { stateChange } from './store/claudio/claudio-reducer/claudio-reducer.component';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore({ count: counterReducer, state: stateChange }),
-  ],
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 };
